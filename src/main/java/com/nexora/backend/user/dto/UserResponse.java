4c76e6a0063c0274248656f712bdd62d20fc2bf0
anjana732 <anjanasingh1257@gmail.com>
2026-09-17 15:03:52 +0530

    Add User response

--- src/main/java/com/nexora/backend/user/dto/UserResponse.java
+++ src/main/java/com/nexora/backend/user/dto/UserResponse.java
@@ -1,4 +1,33 @@
 package com.nexora.backend.user.dto;
 
-public class UserResponse {
+import com.nexora.backend.user.User;
+import java.time.Instant;
+
+public record UserResponse (
+    Long id,
+    String name,
+    String email,
+    String timezone,
+    String profilePicture,
+    String role,
+    String accountStatus,
+    Instant lastLoginAt,
+    Instant createdAt,
+    Instant updatedAt
+    ){
+
+    public static UserResponse from(User user){
+        return new UserResponse(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getTimezone(),
+                user.getProfilePicture(),
+                user.getAccountStatus(),
+                user.getRole(),
+                user.getLastLoginAt(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
 }
